118053b89daab1b1bb5c2cf341adca1dc261ba81
Aaron Bester <bestera@uoguelph.ca>
2020-10-20 17:52:59 -0400

    Got every basic function done

--- tree.c
+++ tree.c
@@ -29,8 +29,46 @@ void attachNode( struct Performance*performance, struct Node**node_ptr,void *src
     newNode->lt = NULL;
 
     *node_ptr = newNode;
+    performance->mallocs++;
+    performance->writes++;
 }
 
 int comparNode( struct Performance *performance, struct Node**node_ptr, int (*compar)(const void *, const void *),void *target){
-    
+    struct Node * currentNode = *node_ptr;
+    return((*compar)(target,currentNode->data));
+}
+
+struct Node**next( struct Performance *performance, struct Node**node_ptr, int direction){
+    struct Node * currentNode = *node_ptr;
+    struct Node ** nextNode = NULL;
+    if(currentNode == NULL){
+         printf(stderr,"Tree is empty");
+    }
+
+    if(direction >= 0){
+        *nextNode = currentNode->gte;
+    }
+    else{
+        *nextNode = currentNode->lt;
+    }
+    performance->reads++;
+    return(nextNode);
+}
+
+void readNode( struct Performance *performance, struct Node **node_ptr, void *dest, unsigned int width ){
+    struct Node * currentNode = *node_ptr;
+    if(currentNode == NULL){
+        printf(stderr,"Tree is empty");
+    }
+
+    memcpy(dest,currentNode->data,width);
+    performance->reads++;
+}
+
+void detachNode(struct Performance *performance, struct Node**node_ptr){
+    struct Node * nodeToBeFreed = *node_ptr;
+
+    free(nodeToBeFreed->data);
+    free(nodeToBeFreed);
+    *node_ptr = NULL;
 }
